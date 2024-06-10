72771060becb47ea928175f34918ea05465d8819
Rishu raj <21je0769@iitism.ac.in>
2024-06-10 14:10:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ let weather = {
         navigator.geolocation.getCurrentPosition(success, console.error);
       }
       else {
-        weather.fetchWeather("Manipal");
+        weather.fetchWeather("Dhanbad");
       }
     }
   };
